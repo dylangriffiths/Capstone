ff03b5dcccf4f4479ed0d6fde5d32fe8658cdc00
dylangriffiths <111248915+dylangriffiths@users.noreply.github.com>
2023-01-25 15:23:27 -0700

    images fixed

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets/styles");
+  eleventyConfig.addPassthroughCopy("src/assets/images");
   return {
       markdownTemplateEngine: 'njk',
       dataTemplateEngine: 'njk',
